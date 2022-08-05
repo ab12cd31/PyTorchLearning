1020bbddc68798e613c1b6190e6d0c041d2aa351
ab12cd31 <yxiaoshen@163.com>
2022-08-06 00:48:15 +0800

    DailyLearning8.6

---
+++ 2.梯度下降算法/Gradient Descent.py
@@ -0,0 +1,38 @@
+"""
+PyCharm Gradient Descent
+2022.08.05
+by SimonYang
+"""
+
+x_data = [1.0, 2.0, 3.0]
+y_data = [2.0, 4.0, 6.0]
+
+w = 1.0
+
+
+def forward(x):
+    return x * w
+
+
+def cost(xs, ys):
+    cost = 0
+    for x, y in zip(xs, ys):
+        y_pred = forward(x)
+        cost += (y_pred - y) ** 2
+    return cost / len(xs)
+
+
+def grident(xs, ys):
+    grad = 0
+    for x, y in zip(xs, ys):
+        grad += 2 * x * (x * w - y)
+    return grad / len(xs)
+
+
+print('Predict (before training)', 4, forward(4))
+for epoch in range(100):
+    cost_val = cost(x_data, y_data)
+    grad_val = grident(x_data, y_data)
+    w -= 0.01 * grad_val
+    print('Epoch:', epoch, 'w=', w, 'loss=', cost_val)
+print("Predict (after training)", 4, forward(4))
