887b4cf1d82378971af4d8aef8ce4f288dd33617
ab12cd31 <yxiaoshen@163.com>
2022-08-13 23:29:50 +0800

    DailyLearning8.13

--- 6.处理多维特征的输入/Multiple Dimension Input.py
+++ 6.处理多维特征的输入/Multiple Dimension Input.py
@@ -3,11 +3,55 @@ PyCharm Multiple Dimension Input
 2022.08.13
 by SimonYang
 """
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+xy = np.loadtxt('diabetes.csv', delimiter=',', dtype=np.float32)
+x_data = torch.from_numpy(xy[:, :-1])
+y_data = torch.from_numpy(xy[:, [-1]])
 
 
-def main():
-    return None
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.Linear1 = torch.nn.Linear(8, 6)
+        self.Linear2 = torch.nn.Linear(6, 4)
+        self.Linear3 = torch.nn.Linear(4, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.sigmoid(self.Linear1(x))
+        x = self.sigmoid(self.Linear2(x))
+        x = self.sigmoid(self.Linear3(x))
+        return x
 
 
-if __name__ == "__main__":
-    main()
+model = Model()
+
+criterion = torch.nn.BCELoss(reduction='mean')
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+epoch_list=[]
+loss_list=[]
+
+
+for epoch in range(100):
+    y_pred=model(x_data)
+    loss=criterion(y_pred,y_data)
+    print(epoch, loss.item())
+    epoch_list.append(epoch)
+    loss_list.append(loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+
+    optimizer.step()
+
+
+
+plt.plot(epoch_list, loss_list)
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.show()
+
+
