c25cf2fe58d540dd142db379af6fba383fb663fd
ab12cd31 <yxiaoshen@163.com>
2022-08-19 08:37:11 +0800

    DailyLearning8.19

---
+++ 10.卷积神经网络(高级篇)/Advanced CNN.py
@@ -0,0 +1,13 @@
+"""
+PyCharm Advanced CNN
+2022.08.18
+by SimonYang
+"""
+
+
+def main():
+    return None
+
+
+if __name__ == "__main__":
+    main()
---
+++ 9.卷积神经网络(基础篇)/homework (CPU).py
@@ -0,0 +1,120 @@
+"""
+PyCharm homework (CPU)
+2022.08.18
+by SimonYang
+"""
+
+import torch
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+batch_size = 64
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+train_dataset = datasets.MNIST(root='../dataset/mnist/',
+                               train=True,
+                               download=True,
+                               transform=transform)
+train_loader = DataLoader(train_dataset,
+                          shuffle=True,
+                          batch_size=batch_size)
+
+test_dataset = datasets.MNIST(root='../dataset/mnist',
+                              train=False,
+                              download=True,
+                              transform=transform)
+test_loader = DataLoader(test_dataset,
+                         shuffle=True,
+                         batch_size=batch_size)
+
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5)
+        self.conv3 = torch.nn.Conv2d(20, 40, kernel_size=3)
+        self.pooling = torch.nn.MaxPool2d(2)
+        self.fc1 = torch.nn.Linear(40, 60)
+        self.fc2 = torch.nn.Linear(60, 30)
+        self.fc3 = torch.nn.Linear(30, 10)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = F.relu(self.pooling(self.conv1(x)))
+        x = F.relu(self.pooling(self.conv2(x)))
+        x = F.relu(self.pooling(self.conv3(x)))
+        x = x.view(batch_size, -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+
+
+model = Net()
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+
+def train(epoch):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader, 0):
+        inputs, target = data
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if batch_idx % 300 == 299:
+            print('[%d, %5d] loss: %3f' % (epoch + 1, batch_idx + 1, running_loss / 300))
+            loss_list.append(running_loss / 300)
+            running_loss = 0.0
+
+
+def test():
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, dim=1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('Accuracy on test set: %d %%' % (100 * correct / total))
+    accurate_list.append(correct / total * 100)
+
+
+if __name__ == '__main__':
+    loss_list = []
+    accurate_list = []
+
+    for epoch in range(10):
+        train(epoch)
+        test()
+
+    num_list = [i for i in range(len(loss_list))]
+    plt.plot(num_list, loss_list)
+    plt.xlabel('i')
+    plt.title('Loss', fontsize=20)
+    plt.ylabel('loss')
+    plt.grid(ls='--')
+    plt.show()
+
+    num_list = [i for i in range(len(accurate_list))]
+    plt.plot(num_list, accurate_list)
+    plt.xlabel('i')
+    plt.grid(ls='--')
+    plt.title('Accurate', fontsize=20)
+    plt.ylabel('accurate')
+    plt.show()
